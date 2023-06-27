825e5d9babde13c2e4ae472c2d57c91eb153e6d4
Robin.Wang <3Jg$94sbQ@q1>
2023-06-27 14:03:07 +0800

    chore(base url): change

--- ui.py
+++ ui.py
@@ -1,19 +1,63 @@
 import streamlit as st
+import os
+from langchain.chat_models import ChatOpenAI
+
+MAX_CONTEXT = 1000
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_BASE = os.getenv("BASE_URL")
+MODEL_NAME = "gpt-3.5-turbo"
 
 st.set_page_config(
     page_title="Regulus Chatbot",
     page_icon=":robot:",
     menu_items={"about": '''
                 Author: Robin.Wang
-                
+
                 Model: ChatGPT-3.5-tubo
                 '''}
 )
 
 st.title("Regulus Chatbot👋")
-ctx_dom = st.empty()
+history_dom = st.empty()
 question_dom = st.markdown(
     ">  回答由 AI 生成，不保证准确率，仅供参考学习！"
 )
-md_dom = st.empty()
+answer_dom = st.empty()
 st.write("")
+
+
+@st.cache_resource
+def get_chat():
+    chat = ChatOpenAI(
+        openai_api_key=OPENAI_API_KEY,
+        openai_api_base=OPENAI_API_BASE,
+        temperature=0,
+        model_name=MODEL_NAME
+    )
+    return chat
+
+
+chat = get_chat()
+
+
+def predict(input):
+    return chat.predict(input)
+
+
+with st.form("form", True):
+    # create a prompt text for the text generation
+    user_input = st.text_area(label=":thinking_face: 咨询点什么？",
+                              height=100,
+                              max_chars=MAX_CONTEXT,
+                              placeholder="支持使用 Markdown 格式书写")
+    col1, col2 = st.columns([1, 1])
+    with col1:
+        btn_send = st.form_submit_button(
+            "发送", use_container_width=True, type="primary")
+    with col2:
+        btn_clear = st.form_submit_button("清除历史记录", use_container_width=True)
+
+    if btn_send and user_input != "":
+        answer = predict(user_input)
+        print(f"回答：{answer}", flush=True)
+        answer_dom.markdown(answer)
