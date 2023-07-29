2818a146e8e2c350719ef8f9fa1ebbc86c79b432
Robin.Wang <3Jg$94sbQ@q1>
2023-07-29 17:50:10 +0800

    feat(teacher): git add frontend build

---
+++ st_audiorec/frontend/build/precache-manifest.ebfe564a2792dbf43ce440c7e7ea59ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2046858121e98d48be156a10f1d51b43",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5b89984570b0567a7cc5",
+    "url": "./static/css/2.bfbf028b.chunk.css"
+  },
+  {
+    "revision": "5b89984570b0567a7cc5",
+    "url": "./static/js/2.82aa384d.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.82aa384d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10240b6e36bab4afe0d",
+    "url": "./static/js/main.1f6ecb4f.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
